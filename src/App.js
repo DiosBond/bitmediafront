aaf1af09af7175eb175aff78053a0c205aad6fd9
dios <dios@localhost.localdomain>
2020-06-23 00:41:38 +0300

    change chart function to class and get paramsfrom route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 //import './css/App.css';
 import './css/style.css';
 
@@ -24,7 +24,7 @@ function App() {
     <Switch>
       <Route exact path='/' component={Main}></Route>
       <Route exact path='/stat' component={Stat}></Route>
-      <Route exact path='/chart' component={Chart}></Route>
+      <Route path='/user/:id' component={Chart}></Route>
     </Switch>
 
     </div>
--- src/Charts.js
+++ src/Charts.js
@@ -1,15 +1,20 @@
 import React from 'react';
-
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import './css/style.css';
 
 
-function Charts() {
-  return (
-    
+class Charts extends React.Component {
+
+  render(){
+    return (
+      <div> 
       <div> TEXT CHART</div>
-    
+      <h2>{this.props.match.params.id}</h2>
+      </div> 
   );
+  }
+
 }
 
 export default Charts;
